1d2bb865816fffbbd4a51342eee69973a1ea974c
Atif Javed <atifjaved999@gmail.com>
2015-08-02 09:42:44 +0500

    Handle cancan error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,11 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
+
+  # To handle cancan Error
+  before_filter do
+     resource = controller_name.singularize.to_sym
+     method = "#{resource}_params"
+     params[resource] &&= send(method) if respond_to?(method, true)
+   end
 end
